2d8c985fa37b0c2abd9c0d762e7de05bf4a02465
jacksoncalvert <jcalvertlane97@gmail.com>
2022-02-06 23:43:54 +1000

    [FIX] Update Test object

--- test/test_client.jl
+++ test/test_client.jl
@@ -1,5 +1,5 @@
 @testset "client" begin
     client = SearchClient("idx")
     @test client.index_name == "idx"
-    @test typeof(client.clinet) <: Jedis.Client    
+    @test typeof(client.client) <: Jedis.Client    
 end
